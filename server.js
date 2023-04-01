b4c05eaa9eb81d0cf2c848e72d875874487b987f
DorTeri <dortayari@gmail.com>
2023-04-01 12:45:39 +0300

    Added delete

--- server.js
+++ server.js
@@ -124,7 +124,6 @@ app.post('/api/openai', async (req, res) => {
         max_tokens: 1000,
         temperature: 0
     })
-    console.log('response.data', response.data)
     if (response.data.choices) {
         res.json({
             message: response.data.choices[0].text
