deb2cd0b09c1944e2897090edf4cf774be81986e
DorTeri <dortayari@gmail.com>
2023-04-03 10:05:16 +0300

    Updated labels order

--- api/board/board.service.js
+++ api/board/board.service.js
@@ -58,6 +58,7 @@ async function add(board) {
 async function update(board) {
     try {
         const boardToSave = {
+            labelsOrder: board.labelsOrder,
             owner:board.owner,
             title: board.title,
             labels:board.labels,
